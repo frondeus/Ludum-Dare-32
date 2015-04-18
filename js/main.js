26b8d246d0dc393deb86e8249bc06ef48b95fd28
Frondeus <frondeus@gmail.com>
2015-04-18 20:43:19 +0200

    Passwords

--- js/main.js
+++ js/main.js
@@ -6,54 +6,57 @@ Engine = {
 		y: 8,
 	},
 
+	players: [],
 	enemies: [],
+	go: [],
+	ground: [],
+	block: [],
 
-};
-var app = playground({
-	scale: 2,
-	smoothing: false,
-
-	create: function(){
-		this.loadAtlases("ground", "characters");
+	addGo: function(){
+		var go = new Engine.GO(arguments[0]);
+		for(var i = 1; i < arguments.length; i++)
+			arguments[i][arguments[i].length] = go;
+		return go;
 	},
 
-	ready: function(){
-		this.go = [];
-
-		for(var x = 0; x < 8; x++)
-			for(var y = 0; y < 8; y++)
-				this.addGO({x: x, y: y});
-
-		this.player = this.addGO({x: 4, y: 4}).addComponent("player", new Engine.Player());
-
-		var enemy = this.addGO({x: 6, y: 6}).addComponent("enemy", new Engine.Enemy());
-		Engine.enemies[Engine.enemies.length] = enemy;
-	},
-
-	addGO: function(args){
-		this.go[this.go.length] = new Engine.GO(args);
-		return this.go[this.go.length-1];
-	},
-
-	render: function(dt){
-		this.layer.clear("#224");
-		for(var i in this.go)
-			this.go[i].render(dt);
+	isGo: function(collection, x, y){
+		for(var i in collection)
+			if(x == collection[i].x && y == collection[i].y) return collection[i];
+		return null;
 	},
 
 	turn: function(){
-		for(var i in Engine.enemies)
-			Engine.enemies[i].enemy.turn();
+		for(var i in this.enemies)
+			this.enemies[i].enemy.turn();
+	},
+};
+var app = playground({
+	smoothing: false,
+
+	create: function(){
+		this.loadAtlases("map", "characters", "ui");
 	},
 
-	keyup: function(event){
+	ready: function(){
+
+		for(var x = 0; x < 8; x++)
+			for(var y = 0; y < 8; y++)
+				Engine.addGo({x: x, y: y}, Engine.go, Engine.ground);
+
+		Engine.player = Engine.addGo({x: 4, y: 4}, Engine.go, Engine.block, Engine.players).addComponent("player", new Engine.Player());
+
+		Engine.addGo({x: 6, y: 6}, Engine.go, Engine.enemies, Engine.block ).addComponent("enemy", new Engine.Enemy());
+
+		Engine.addGo({x: 0, y: 2, frame: 1}, Engine.go, Engine.block);
+		Engine.addGo({x: 1, y: 2, frame: 1}, Engine.go, Engine.block);
+		Engine.addGo({x: 2, y: 2, frame: 1}, Engine.go, Engine.block);
+		Engine.addGo({x: 3, y: 2, frame: 1}, Engine.go, Engine.block);
+
+		Engine.addGo({x: 5, y: 2, frame: 1}, Engine.go, Engine.block);
+		Engine.addGo({x: 6, y: 2, frame: 1}, Engine.go, Engine.block);
+		Engine.addGo({x: 7, y: 2, frame: 1}, Engine.go, Engine.block);
 		
-		switch(event.key){
-			default:
-				this.player.player.input(event.key);
-				break;
-		}
-		this.turn();
-	}
+		this.setState(Engine.Map);
+	},
 });
 
--- js/player.js
+++ js/player.js
@@ -1,26 +1,38 @@
 Engine.Player = function(){
-
 };
 
 Engine.Player.prototype = {
 	init: function(){
 		this.go.tileset = app.atlases.characters;
+		this.go.lifes = 1;
 	},
 
-	input: function(key){
-		switch(key){
-			case 'up':
-				this.go.y--;
-				break;
-			case 'down':
-				this.go.y++;
-				break;
-			case 'left':
-				this.go.x--;
-				break;
-			case 'right':
-				this.go.x++;
-				break;
+	canGo: function(x, y){
+		if(Engine.isGo(Engine.enemies,x,y)) {
+			app.setState(Engine.Fight);
 		}
+		if(Engine.isGo(Engine.block,x,y)) return false;
+		if(Engine.isGo(Engine.ground,x,y)) return true;
+		return false;
+	},
+
+	input: function(x,y){
+		var nextX = this.go.x;
+		var nextY = this.go.y;
+		
+		if(Math.abs(x - this.go.x) <= 1 && Math.abs(y - this.go.y) <= 1){
+			if(Math.abs(x - this.go.x) <= 1)	
+				nextX = x;
+			if(Math.abs(y - this.go.y) <= 1)
+				nextY = y;
+		}
+
+		if(this.canGo(nextX, nextY)){
+			app.tween(this.go)
+				.to({x: nextX, y: nextY}, 0.1, "01")
+				.play();
+		}
+
+		Engine.turn();
 	}
 };
