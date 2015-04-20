d210a62892bfac18430bde5c015aeb98e11ac75e
Frondeus <frondeus@gmail.com>
2015-04-20 17:44:41 +0200

    Mazes

--- js/generator.js
+++ js/generator.js
@@ -5,7 +5,7 @@ Engine.generateMap = function(){
 	Engine.generator.mapH = Utils.randomZ(8,16);
 	Engine.generator.roomSize = Utils.randomZ(8,16);
 	Engine.generator.roomCount = Utils.randomZ(8,32);
-	Engine.generator.maxMazeLength = Utils.randomZ(8,16);
+	Engine.generator.maxMazeLength = Utils.randomZ(16,32);
 	Engine.generator.makeRooms();
 
 	Engine.generator.spawnPlayer();
@@ -28,41 +28,60 @@ Engine.generator = {
 		this.makeMazes();
 	},
 
+	makeEdge: function(x, y){
+		Engine.addGo(new Engine.GO({x: x, y: y, frame: 1, /*tileset: app.atlases.ui*/}), Engine.go, Engine.block, this.edges);
+	},
+
 	makeMaze: function(x, y){
 		var id = ++this.uniqueId;
-		var stack = [];
+		var stack = [[x, y]];
 		var length = 1;
 
+		this.zones[id] = [];
+
 		Engine.addGo(new Engine.GO({x: x, y: y, id: id}), Engine.go, Engine.ground, this.zones[id]);
+		
+		for(_x = -1; _x <= 1; _x++)
+			for(_y = -1; _y <= 1; _y++)
+				if(!Engine.isGo(Engine.go,x + _x, y + _y )&& (_x != 0 || _y != 0))
+					this.makeEdge(x + _x, y + _y);
+
 		var dirs = [[0,1], [0,-1], [-1,0], [1,0]];
 		var _x, _y;
 
 		while(stack.length > 0 && length < this.maxMazeLength){
+			for(var loop = 0; loop < this.maxLoop; loop++){
+				var dir = dirs[Utils.randomZ(0,dirs.length-1)];
+				var edge = Engine.isGo(this.edges, x + dir[0], y + dir[1]);
+				if(edge){
+					edge.frame = 0;
+					edge.tileset = app.atlases.map;
+
+					Engine.removeGo(edge, this.edges, Engine.block);
+					Engine.addGo(edge, Engine.ground, this.zones[id]);
+			 		stack[stack.length] = [x + dir[0], y + dir[1]];
+					length ++;
+					break;
+				}
+			}
 			x = stack[stack.length-1][0];
 			y = stack[stack.length-1][1];
 
 			for(_x = -1; _x <= 1; _x++)
 				for(_y = -1; _y <= 1; _y++)
-					if(!Engine.isGo(this.edges,x,Y-1))
-						Engine.addGo(new Engine.GO({x: x + _x, y: y + _y, frame: 1, zIndex: 2}), Engine.go, Engine.block, this.edges);
+					if(!Engine.isGo(Engine.go,x + _x, y + _y )&& (_x != 0 || _y != 0))
+						this.makeEdge(x + _x, y + _y);
 
-			for(var loop = 0; loop < this.maxLoop; loop++){
-				var _dir = dirs[Utils.randomZ(0,3)];
-				_x = stack[stack.length-1][0] + _dir[0];
-				_y = stack[stack.length-1][1] + _dir[1];
-				if(Engine.isGo(this.edges, _x, _y)){
-					var __x, __y, free = true;
-					for(__x = -1; __x <= 1; __x++)
-				}
-			}
 			stack.splice(0,1);
 		}
+		// console.log("-");
 	},
 
 	makeMazes: function(){
+		var x, y;
 		for(x = Math.floor(-this.mapW / 2); x < Math.floor(this.mapW * 1.5); x++)
 			for(y = Math.floor(-this.mapH / 2); y < Math.floor(this.mapH * 1.5); y++)
-				if(!Engine.isGo(Engine.ground, x + _x, y + _y))
+				if(!Engine.isGo(Engine.go, x, y))
 					this.makeMaze(x,y);
 	},
 
@@ -91,16 +110,17 @@ Engine.generator = {
 
 				for(x = X-1; x< W+1; x++){
 					if(!Engine.isGo(this.edges,x,Y-1))
-						Engine.addGo(new Engine.GO({x: x, y: Y-1, frame: 1, zIndex: 2}), Engine.go, Engine.block, this.edges);
+						this.makeEdge(x,Y-1);
 					if(!Engine.isGo(this.edges,x,H))
-						Engine.addGo(new Engine.GO({x: x, y: H, frame: 1, zIndex: 2}), Engine.go, Engine.block, this.edges);
+						this.makeEdge(x,H);
 				}
 
 				for(y = Y-1; y < H+1; y++){
 					if(!Engine.isGo(this.edges,X-1,y))
-						Engine.addGo(new Engine.GO({x: X-1, y: y, frame: 1, zIndex: 2}), Engine.go, Engine.block, this.edges);
+						this.makeEdge(X-1,y);
 					if(!Engine.isGo(this.edges,W,y))
-						Engine.addGo(new Engine.GO({x: W, y: y, frame: 1, zIndex: 2}), Engine.go, Engine.block, this.edges);	
+						this.makeEdge(W,y);
+						
 				}
 
 				this.rooms[this.rooms.length] = {X: X, Y: Y, W: W, H: H};
